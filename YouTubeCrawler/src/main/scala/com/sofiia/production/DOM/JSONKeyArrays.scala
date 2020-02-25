182a78c15e5ad1afd6558f6f3aa0bd171c5e100e
Sofiia Materynska <sofiia.materynska@sigma.software>
2020-02-25 19:40:12 +0200

    Added i/o functions

--- YouTubeCrawler/src/main/scala/com/sofiia/production/DOM/JSONKeyArrays.scala
+++
@@ -1,9 +0,0 @@
-package com.sofiia.production.DOM
-
-object JSONKeyArrays {
-  val title=Array("snippet", "title")
-  val likes=Array("statistics", "likeCount")
-  val dislikes=Array("statistics", "dislikeCount")
-  val views=Array("statistics", "viewCount")
-  val chanelTitle=Array("snippet", "channelTitle")
-}
