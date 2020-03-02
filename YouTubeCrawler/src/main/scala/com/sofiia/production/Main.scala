1634839f9321f110c45c1a740332051416c98103
Sofiia Materynska <sofiia.materynska@sigma.software>
2020-03-02 15:55:53 +0200

    Parallel solution with threads added

--- YouTubeCrawler/src/main/scala/com/sofiia/production/Main.scala
+++ YouTubeCrawler/src/main/scala/com/sofiia/production/Main.scala
@@ -1,19 +1,47 @@
 package com.sofiia.production
 
+import java.util.concurrent.Executors
+
 import com.sofiia.production.functions.io.{FileReader, FileWriterCustom}
-import com.sofiia.production.functions.{YTVideoListParser, YouTubeCrawler}
+import com.sofiia.production.functions.{YTVideoListParser, YouTubeVideoCrawler}
 import com.google.api.services.youtube.model.VideoListResponse
 import com.sofiia.production.functions.YouTubeCrawler.Region
 
 object Main {
+  val videosIDArray=Array("wkPR4Rcf4ww", "WNeLUngb-Xg", "rR4n-0KYeKQ", "8kooIgKESYE", "_Yhyp-_hX2s" )
 
    def main(args: Array[String]) {
-     val fileName=args(0)
-     val APIKey=args(1)
-     val amountOfResults=args(2)
-     val response : VideoListResponse = new YouTubeCrawler()
+     val properties: Properties = new Properties()
+     properties.load(new FileInputStream(args(0)))
+
+     val fileLocation=properties.getProperty("videoByIdDirectory")
+     val mostPopularVideosFileName = properties.getProperty("mostPopularVideoFile")
+     val APIKey = properties.getProperty("APIKey")
+     val amountOfResults = properties.getProperty("numberOfVideos")
+     val fileNameForFuture = properties.getProperty("fileNameForFuture")
+
+     val response: VideoListResponse = new YouTubeVideoCrawler()
        .requestOnMostPopularVideo(APIKey, amountOfResults.toInt, Region.UnitedStates)
-     FileWriterCustom.write(fileName, response.toPrettyString)
-     new YTVideoListParser().fromJson(FileReader.read(fileName))
+     FileWriterCustom.write(mostPopularVideosFileName, response.toPrettyString)
+     new YTVideoListParser().fromJson(FileReader.read(mostPopularVideosFileName))
+
+     parallelDataCrawlingToFiles(APIKey, YTVideoJson.fileLocation)
    }
+
+  def parallelDataCrawlingToFiles(APIKey: String, fileLocation: String) : Unit = {
+    val pool = Executors.newFixedThreadPool(2)
+
+    videosIDArray.foreach { item =>
+      pool.execute(
+        new Runnable {
+          def run {
+            val video = new YouTubeVideoCrawler().getVideoDataById(APIKey, item)
+            FileWriterCustom
+              .write(fileLocation + videosIDArray.indexOf(item) + ".json", video.toPrettyString)
+          }
+        }
+      )
+    }
+    pool.shutdown()
+  }
 }
---
+++ YouTubeCrawler/src/main/scala/com/sofiia/production/functions/YouTubeVideoCrawler.scala
@@ -0,0 +1,19 @@
+package com.sofiia.production.functions
+
+import com.google.api.services.youtube.model.{Video, VideoListResponse}
+import com.sofiia.production.functions.YouTubeCrawler.Region.Region
+
+class YouTubeVideoCrawler {
+  val YTCrawler = new YouTubeCrawler()
+
+  def requestOnMostPopularVideo(APIKey: String, numberOfResults: Long, region: Region):  VideoListResponse={
+    val request = YTCrawler.connection(APIKey).videos.list("snippet,contentDetails,statistics").setMaxResults(numberOfResults)
+    request.setChart("mostPopular").setRegionCode(region.toString).execute
+  }
+
+  def getVideoDataById(APIKey:String, Id: String): Video = {
+    val videoResponse = YTCrawler.connection(APIKey).videos().list("snippet").setId(Id).execute()
+    videoResponse.getItems.get(0)
+
+  }
+}
