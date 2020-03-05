7a73dc64e6cd16259650ae23e371c3c08dcd4cc8
Sofiia Materynska <sofiia.materynska@sigma.software>
2020-03-05 20:54:51 +0200

    Parallel solution with Future added

--- YouTubeCrawler/src/main/scala/com/sofiia/production/Main.scala
+++ YouTubeCrawler/src/main/scala/com/sofiia/production/Main.scala
@@ -1,6 +1,8 @@
 package com.sofiia.production
 
-import java.util.concurrent.Executors
+import java.io.FileInputStream
+import java.util.Properties
+import java.util.concurrent.{Callable, Executors, FutureTask}
 
 import com.sofiia.production.functions.io.{FileReader, FileWriterCustom}
 import com.sofiia.production.functions.{YTVideoListParser, YouTubeVideoCrawler}
@@ -25,7 +27,8 @@ object Main {
      FileWriterCustom.write(mostPopularVideosFileName, response.toPrettyString)
      new YTVideoListParser().fromJson(FileReader.read(mostPopularVideosFileName))
 
-     parallelDataCrawlingToFiles(APIKey, YTVideoJson.fileLocation)
+     parallelDataCrawlingToFiles(APIKey, fileLocation)
+     parallelDataCrawlingWithFutureToFile(APIKey, fileNameForFuture)
    }
 
   def parallelDataCrawlingToFiles(APIKey: String, fileLocation: String) : Unit = {
@@ -43,5 +46,23 @@ object Main {
       )
     }
     pool.shutdown()
+  }
+
+  def parallelDataCrawlingWithFutureToFile(APIKey: String, fileLocation: String) : Unit = {
+    val pool = Executors.newFixedThreadPool(5)
+
+    val newList = videosIDArray.map { item =>
+      val future = new FutureTask[String] (new Callable[String] {
+      def call: String = {
+        val video = new YouTubeVideoCrawler().getVideoDataById(APIKey, item)
+        video.toPrettyString
+      }
+    })
+      pool.execute(future)
+      future.get()
+    }.toList
+
+    pool.shutdown()
+    FileWriterCustom.writeFromListToJson(fileLocation, newList)
   }
 }
